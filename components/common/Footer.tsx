c0d65eef47890b95a01b0cd07c267af7fb663205
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-14 23:03:28 +0530

    add footer without responsive

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,7 +1,81 @@
 import React from "react";
+import { styles } from "./style";
+import Link from "next/link";
+import { footerData } from "./Helper";
+import { InstaIcon, LinkedinIcon, YoutubeIcon } from "./Icons";
 
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-gray-300 py-10">
+      <div className={styles.container}>
+        <img src="/img/logo.png" className="w-[100px]" alt="" />
+        <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-3 mt-3">
+          {footerData.map((section, index) => (
+            <div key={index}>
+              <h3 className="text-white text-[10.635px] leading-[164.555% ] font-inter font-semibold mb-[9.4px]">
+                {section.title}
+              </h3>
+              <ul className="space-y-[9.4px] ">
+                {section.links.map((link, linkIndex) => (
+                  <li key={linkIndex}>
+                    <Link
+                      href={link.href}
+                      className="text-[9.219px] font-light leading-[147.899% ] hover:underline duration-300 ease-in-out transition-all"
+                    >
+                      {link.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div>
+            <h3 className="text-white text-[10.635px] leading-[164.555% ] font-inter font-semibold mb-[9.4px]">
+              Get the Latest Insights
+            </h3>
+            <p className="text-[9.219px] font-light leading-[147.899% ] hover:underline duration-300 ease-in-out transition-all">
+              Subscribe to our newsletter and receive cutting-edge digital
+              advertising insights delivered right to your inbox.
+            </p>
+            <form>
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full px-3 py-2 mb-4 !bg-transparent text-gray-900 rounded-md"
+              />
+              <button
+                type="submit"
+                className="w-full px-4 py-2 bg-gray-800 text-white rounded-md hover:bg-gray-700"
+              >
+                Subscribe
+              </button>
+            </form>
+            <div className="flex space-x-4 mt-4">
+              <Link href={""}>
+                <LinkedinIcon />
+              </Link>
+              <Link href={""}>
+                <YoutubeIcon />
+              </Link>
+              <Link href={""}>
+                <InstaIcon />
+              </Link>
+
+              <a href="#" className="hover:text-purple-500">
+                <i className="fab fa-youtube"></i>
+              </a>
+              <a href="#" className="hover:text-purple-500">
+                <i className="fab fa-xing"></i>
+              </a>
+              <a href="#" className="hover:text-purple-500">
+                <i className="fab fa-tiktok"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ components/common/Helper.tsx
@@ -0,0 +1,44 @@
+export const footerData = [
+  {
+    title: "Advertisers",
+    links: [
+      { name: "Advertiser Solutions", href: "#" },
+      { name: "Inventory Packages & PMP’s", href: "#" },
+      { name: "Case Studies", href: "#" },
+      { name: "Support", href: "#" },
+    ],
+  },
+  {
+    title: "Publishers",
+    links: [
+      { name: "Publisher Solutions", href: "#" },
+      { name: "Case Studies", href: "#" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { name: "About", href: "#" },
+      { name: "Leadership", href: "#" },
+      { name: "Careers", href: "#" },
+      { name: "Support", href: "#" },
+      { name: "Privacy Center", href: "#" },
+      { name: "Opt-Out/Do Not Sell Personal Info", href: "#" },
+      { name: "Contact", href: "#" },
+      { name: "Copyright Notice", href: "#" },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { name: "Blog", href: "#" },
+      { name: "Case Studies", href: "#" },
+      { name: "Press", href: "#" },
+      { name: "Supply Policy", href: "#" },
+      { name: "Demand Policy", href: "#" },
+      { name: "Marketplace Quality Policy", href: "#" },
+      { name: "Terms of Service", href: "#" },
+      { name: "Brand Guide", href: "#" },
+    ],
+  },
+];
