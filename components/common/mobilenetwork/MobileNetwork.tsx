0b09a1ec01e693a460c725820bcadb998e15c2c3
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-15 12:26:16 +0530

    add netwrok section

--- components/common/mobilenetwork/MobileNetwork.tsx
+++ components/common/mobilenetwork/MobileNetwork.tsx
@@ -2,15 +2,13 @@ import Image from "next/image";
 import React from "react";
 import { networks } from "../Helper";
 
-
-
 const MobileNetwork = () => {
   return (
-    <section className="px-4 max-w-[1007px] w-full mx-auto py-6">
+    <section className="px-4 max-w-[90%] lg:max-w-[1007px] w-full mx-auto py-6">
       <h3 className="text-[12.027px] sm:text-base md:text-xl lg:text-2xl font-bold font-lato text-center text-thunder">
         $5M Revenue Boost Across Our Media Network
       </h3>
-      <div className="grid place-content-center place-items-center grid-cols-5 gap-[9px] lg:gap-5 mt-2 sm:mt-5  md:mt-8 lg:mt-10">
+      <div className="grid place-content-center place-items-center grid-cols-5 gap-[9px] lg:gap-5 mt-4 sm:mt-7 lg:mt-10">
         {networks.map((network, index) => (
           <Image
             key={index}
@@ -20,7 +18,6 @@ const MobileNetwork = () => {
             height={99}
           />
         ))}
-    
       </div>
     </section>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,10 @@ export default function Home() {
     <>
       <main className="bg-main-bg ">
         {" "}
-        <Header />
+        {/* <Header />
         <Hero />
         <Amplify />
-        <Platforms />
+        <Platforms /> */}
         <MobileNetwork />
         <Footer />
       </main>
