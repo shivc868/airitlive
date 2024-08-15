0b0d073942838fe055f6971b2a0eec84c833bd81
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-15 07:08:09 +0530

    footer desktop done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Header from "@/components/common/Header";
-import Hero from "@/components/home/Hero";
 import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
 import Amplify from "@/components/home/Amplify";
+import Hero from "@/components/home/Hero";
 import Platforms from "@/components/home/Platforms";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+
 
 export default function Home() {
   return (
     <>
       <main className="bg-main-bg ">
         {" "}
-        {/* <Header />
+        <Header />
         <Hero />
         <Amplify />
-        <Platforms /> */}
+        <Platforms />
         <Footer />
       </main>
     </>
