e8cd5afe9d65c6f2c72925268173340fca07a217
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-15 22:51:28 +0530

    add marquee

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ export default function Home() {
     <>
       <main className="bg-main-bg ">
         {" "}
-        {/* <Header />
+        <Header />
         <Hero />
         <Amplify />
-        <Platforms /> */}
+        <Platforms />
         <MobileNetwork />
         <Testimonial />
         <ContactUs />
