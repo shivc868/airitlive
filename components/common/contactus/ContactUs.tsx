fe911b12724dbb0496fd339199ccca7ff1734e92
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-15 22:55:29 +0530

    add marquee

--- components/common/contactus/ContactUs.tsx
+++ components/common/contactus/ContactUs.tsx
@@ -4,7 +4,7 @@ import ContactForm from "./ContactForm";
 const ContactUs = () => {
   return (
     <section className="bg-lavender py-9 xl:py-12 flex justify-between md:flex-row flex-col gap-5 items-center max-w-[1033px] mx-auto px-4">
-      <div className="lg:max-w-[558px] space-y-5 xl:space-y-14 w-full">
+      <div className="lg:max-w-[558px] space-y-5 md:space-y-10 lg:space-y-14 w-full">
         <h3 className="text-lg sm:text-xl md:text-3xl lg:text-[40px] xl:text-[48px]">
           Ready to transform your media ecosystem?
         </h3>
