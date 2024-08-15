a24eab8c64c1cecf4dca1737695288a5a10e24b3
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-15 22:50:59 +0530

    add marquee

--- components/common/contactus/ContactForm.tsx
+++ components/common/contactus/ContactForm.tsx
@@ -1,9 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ContactForm = () => {
+interface FormData {
+  firstName: string;
+  lastName: string;
+  company: string;
+  email: string;
+}
+
+const ContactForm: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    firstName: "",
+    lastName: "",
+    company: "",
+    email: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log(formData);
+  };
+
   return (
-    <div className="bg-lavenderIndigo p-12 rounded-[29.525px] backdrop-blur-md">
-      <h4>Contact us</h4>
+    <div className="bg-lavenderIndigo p-5 lg:p-12 lg:max-w-[477px] w-full rounded-[19px] lg:rounded-[29.525px] backdrop-blur-md">
+      <h4 className="text-lg font-medium font-lato md:text-2xl mb-6">
+        Contact Us
+      </h4>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4 flex gap-4">
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            className="input-style w-full"
+            placeholder="First Name*"
+            required
+          />
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            className="input-style w-full"
+            placeholder="Last Name*"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="text"
+            id="company"
+            name="company"
+            value={formData.company}
+            onChange={handleChange}
+            className="input-style w-full"
+            placeholder="Company*"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="input-style w-full"
+            placeholder="Work Email*"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className=" rounded-[92.265px] bg-purple text-white text-base font-semibold font-inter px-5 py-2 w-fit transition duration-300"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- components/common/contactus/ContactUs.tsx
+++ components/common/contactus/ContactUs.tsx
@@ -3,10 +3,12 @@ import ContactForm from "./ContactForm";
 
 const ContactUs = () => {
   return (
-    <section className="bg-lavender h-screen flex justify-between items-center max-w-[1033px] mx-auto px-4">
-      <div>
-        <h3 className="">Ready to transform your media ecosystem?</h3>
-        <button>Become a partner </button>
+    <section className="bg-lavender py-9 xl:py-12 flex justify-between md:flex-row flex-col gap-5 items-center max-w-[1033px] mx-auto px-4">
+      <div className="lg:max-w-[558px] space-y-5 w-full">
+        <h3 className="text-lg sm:text-xl md:text-3xl lg:text-[40px] xl:text-[48px]">
+          Ready to transform your media ecosystem?
+        </h3>
+        <button className="btn-primary">Become a partner </button>
       </div>
       <ContactForm />
     </section>
