f674c3371c6bb7b82ffbea9da64ddd5e7f7fcd29
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-16 22:03:20 +0530

    add contact page

--- components/contactpage/ContactHero.tsx
+++ components/contactpage/ContactHero.tsx
@@ -5,7 +5,7 @@ const ContactHero = () => {
     <>
       <header className="px-4">
         <div
-          className={`${styles.container} py-14 md:py-16 sm:pt-40 flex flex-col justify-center items-center text-center gap-4 sm:gap-6 md:gap-9`}
+          className={`${styles.container} py-14 md:py-16 flex flex-col justify-center items-center text-center gap-4 sm:gap-6 md:gap-9`}
         >
           <h1 className="flex flex-col text-[30px] sm:text-[50px] lg:text-[68px] leading-[1] font-helvetica-bold font-bold tracking-[5px] text-center">
             <span className="gradient-hero-main-text text-center leading-[1] ">
