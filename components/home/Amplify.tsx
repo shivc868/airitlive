60cdf59d6d053530a661cdd56c356fe35bbbfa20
Tanu chaudhary <kaurtanu213@gmail.com>
2024-08-22 12:54:53 +0530

    add link in secondary button

--- components/home/Amplify.tsx
+++ components/home/Amplify.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { styles } from "../common/style";
 import { EntertainmentIcon, NewsIcon, SportsIcon } from "../common/Icons";
 import PerformanceMetricsCards from "./PerformanceMetricsCards";
+import SecondaryButton from "../common/SecondaryButton";
 
 const Amplify = () => {
   return (
     <section className="px-4 py-24">
       <div className={styles.container}>
         <div className="flex lg:flex-row flex-col items-center justify-between sm:gap-10 gap-6 md:gap-14 lg:gap-3">
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 lg:gap-12">
             <h2 className="section-heading">Amplify your Content</h2>
             <div className="lg:block hidden">
-              <button className="btn-primary">Know more</button>
+              <SecondaryButton className="!py-1.5 lg:text-xl">
+                Know More
+              </SecondaryButton>
             </div>
           </div>
 
