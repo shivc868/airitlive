31ee2c456d8975974128fb5e90242169ad80df6c
shivc8658 <shiv.kumar@tasolglobal.com>
2024-09-04 16:02:21 +0530

    blog slug added

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/blog/:slug*",
-        destination: "https://your-wordpress-domain.com/blog/:slug*", // External WordPress URL
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/blog/:slug*",
+  //       destination: "https://your-wordpress-domain.com/blog/:slug*", // External WordPress URL
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
